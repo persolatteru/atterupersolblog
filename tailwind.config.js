57a1b75a5b13e5b7d538beee14abfa0d7b9026b6
atsuya.nakada <atsuya.nakada@phr-trgdev.com>
2025-10-02 09:33:41 +0900

    画像サイズの修正

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,7 +15,21 @@ module.exports = {
       },
       fontFamily: {
         sans: ['Inter', 'sans-serif'], // モダンなフォントを指定
-      }
+      },
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            // Markdown内のimgタグの最大幅を親コンテナに合わせる
+            img: {
+              width: '100%',     // 親要素の幅に合わせる
+              maxWidth: '100%',  // 親要素の幅を超えないようにする
+              height: 'auto',    // 縦横比を維持する
+              borderRadius: theme('borderRadius.lg'), // 角を少し丸くする装飾
+              margin: '2rem auto', // 中央寄せと上下マージンを追加
+            },
+          },
+        },
+      }),
     },
   },
   // 💡 修正: Typographyプラグインを追加
